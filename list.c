df4e4cbb63c51180ec09aeaf601f6cf1127a0b6a
Your Name <you@example.com>
2026-03-26 19:59:24 +0000

    new try

--- list.c
+++ list.c
@@ -132,7 +132,6 @@ void * popCurrent(List * list) {
         list->tail = list->current->prev;
         list->current = list->current->prev;
         free(list->current->next);
-        continue;
             
     }
     return NULL;
